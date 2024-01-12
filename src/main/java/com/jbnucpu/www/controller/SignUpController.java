27856b41f03ea03416d7a50c072689283eacb1bc
dohyeon <ehgus8910@gmail.com>
2024-01-13 01:01:56 +0900

    1.0.6

---
+++ src/main/java/com/jbnucpu/www/controller/SignUpController.java
@@ -0,0 +1,7 @@
+package com.jbnucpu.www.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SignUpController {
+}
---
+++ src/main/java/com/jbnucpu/www/service/SignUpService.java
@@ -0,0 +1,7 @@
+package com.jbnucpu.www.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SignUpService {
+}
