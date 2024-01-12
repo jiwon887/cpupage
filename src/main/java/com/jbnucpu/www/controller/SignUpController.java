34b83be30c65cb928f99df4c7c90dee6bedbc39d
dohyeon <ehgus8910@gmail.com>
2024-01-13 01:24:39 +0900

    1.0.8

--- src/main/java/com/jbnucpu/www/controller/SignUpController.java
+++ src/main/java/com/jbnucpu/www/controller/SignUpController.java
@@ -1,7 +1,15 @@
 package com.jbnucpu.www.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SignUpController {
+    @GetMapping("/signup")
+    public String signup(){
+
+        return "signup";
+    }
 }
--- src/main/java/com/jbnucpu/www/dto/SignUpDTO.java
+++ src/main/java/com/jbnucpu/www/dto/SignUpDTO.java
@@ -1,2 +1,21 @@
-package com.jbnucpu.www.dto;public class SignUpDTO {
+package com.jbnucpu.www.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignUpDTO {
+
+    private String name;
+
+    private String studentnumber;
+    private String password;
+
+    private String college; // 단과대학
+    private String department; // 학과
+    private Boolean isregistered; // 재학 상태 (True => 재학, False => 휴학)
+    private String grade;
+
+    private String phonenumber;
 }
