90576d98e3a6320528320e9b2db1ae7be7a251cc
kimjihun <xxxjjhhh@naver.com>
2024-02-05 16:49:45 +0900

    1.0.83

--- src/main/java/com/jbnucpu/www/controller/calendar/CalendarController.java
+++ src/main/java/com/jbnucpu/www/controller/calendar/CalendarController.java
@@ -3,6 +3,7 @@ package com.jbnucpu.www.controller.calendar;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 /**
  * 메인화면 캘린더에 대한 기본적인 API
  * @author 김지훈
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  **/
 
 @Controller
-public class Calendar {
+public class CalendarController {
 
     @GetMapping("/calendar")
     public String calendarAdminPage() {
