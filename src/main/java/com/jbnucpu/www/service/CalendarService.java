1b3422c2518f78cf7c79f5ec94eb9b3da1ae04c4
kimjihun <xxxjjhhh@naver.com>
2024-02-05 17:55:34 +0900

    1.0.88

--- src/main/java/com/jbnucpu/www/service/CalendarService.java
+++ src/main/java/com/jbnucpu/www/service/CalendarService.java
@@ -5,6 +5,10 @@ import com.jbnucpu.www.entity.CalendarEntity;
 import com.jbnucpu.www.repository.CalendarRepository;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -17,9 +21,32 @@ public class CalendarService {
         this.calendarRepository = calendarRepository;
     }
 
+    //add one day logic
+    private String addOneDayForEnd(String end) {
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = null;
+        try {
+            date = format.parse(end);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DATE, 1);
+
+        date = new Date(calendar.getTimeInMillis());
+
+
+        return format.format(date);
+    }
+
     //create new schedule logic
     public void addSchedule(CalendarDTO calendarDTO) {
 
+        String calculateEndDate = addOneDayForEnd(calendarDTO.getEnd());
+        calendarDTO.setEnd(calculateEndDate);
         CalendarEntity newSchedule = calendarDTO.toEntity();
 
         calendarRepository.save(newSchedule);
